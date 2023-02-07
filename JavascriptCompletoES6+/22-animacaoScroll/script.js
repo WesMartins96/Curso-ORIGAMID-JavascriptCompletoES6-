35e83bfe601447e77b5e70d276595f25076fc292
Wesley Martins <wesley.mlopes@castgroup.com>
2023-02-06 22:45:05 -0300

    Animação ao scroll

--- JavascriptCompletoES6+/22-animacaoScroll/script.js
+++ JavascriptCompletoES6+/22-animacaoScroll/script.js
@@ -1,8 +1,14 @@
+//Veremos mais abaixo Animação ao Scroll
+
+//Veremos sobre Scroll suave link interno mais abaixo 
+
+
+//É sempre bom isolar cada código 
 function initTabNav() {
   const tabMenu = document.querySelectorAll('.js-tabmenu li');
   const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-  if(tabMenu.length && tabContent.length) {
+  if (tabMenu.length && tabContent.length) {
     tabContent[0].classList.add('ativo');
 
     function activeTab(index) {
@@ -21,26 +27,31 @@ function initTabNav() {
 }
 initTabNav();
 
+
+
+//Criando accordinList com animação
 function initAccordion() {
   const accordionList = document.querySelectorAll('.js-accordion dt');
-  const activeClass = 'ativo';
-  
-  if(accordionList.length) {
-    accordionList[0].classList.add(activeClass);
-    accordionList[0].nextElementSibling.classList.add(activeClass);
+  if (accordionList.length) {
+    accordionList[0].classList.add('ativo');
+    accordionList[0].nextElementSibling.classList.add('ativo');
 
     function activeAccordion() {
-      this.classList.toggle(activeClass);
-      this.nextElementSibling.classList.toggle(activeClass);
+      this.classList.toggle('ativo');
+      this.nextElementSibling.classList.toggle('ativo');
     }
 
     accordionList.forEach((item) => {
       item.addEventListener('click', activeAccordion);
-    });
+    })
   }
 }
+//Se estivermos repetindo muito um nome, por exemplo a classe 'ativo', podemos coloca-la em
+//uma constante
 initAccordion();
 
+
+//Adicionando Scroll Suave nos links internos
 function initScrollSuave() {
   const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
 
@@ -48,17 +59,16 @@ function initScrollSuave() {
     event.preventDefault();
     const href = event.currentTarget.getAttribute('href');
     const section = document.querySelector(href);
-    section.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-    });
+    const topo = section.offsetTop;
+    //Metodo scrollTo(y-coordenada, x-coordenada);
+    //window.scrollTo(0, 1000)
 
-    // forma alternativa
-    // const topo = section.offsetTop;
-    // window.scrollTo({
-    //   top: topo,
-    //   behavior: 'smooth',
-    // });
+    //Podemos em vez de usarmos o scrollTo com duas coordenadas, podemos usar um objeto
+    //scrollTo(options);
+    window.scrollTo({
+      top: topo,
+      behavior: 'smooth',
+    })
   }
 
   linksInternos.forEach((link) => {
@@ -67,25 +77,28 @@ function initScrollSuave() {
 }
 initScrollSuave();
 
+
+
+//Animação ao Scroll
 function initAnimacaoScroll() {
   const sections = document.querySelectorAll('.js-scroll');
-  if(sections.length) {
+
+  if (sections.length) {
     const windowMetade = window.innerHeight * 0.6;
 
     function animaScroll() {
       sections.forEach((section) => {
-        const sectionTop = section.getBoundingClientRect().top;
-        const isSectionVisible = (sectionTop - windowMetade) < 0;
-        if(isSectionVisible)
+        const sectionTop = section.getBoundingClientRect().top - windowMetade;
+        console.log(sectionTop);
+        if (sectionTop < 0) {
           section.classList.add('ativo');
-        else 
-          section.classList.remove('ativo');
+        }
       })
     }
-
-    animaScroll();
 
     window.addEventListener('scroll', animaScroll);
   }
 }
 initAnimacaoScroll();
+
+

dea2591ddc2c3b807b9766cbaa2084fc4dd92a48
Wesley Martins <wesley.mlopes@castgroup.com>
2023-02-09 01:04:56 -0300

    Constructor Function aula completa

--- JavascriptCompletoES6+/23-constructorFunction/script.js
+++ JavascriptCompletoES6+/23-constructorFunction/script.js
@@ -99,19 +99,100 @@ const yamaha = new Moto('Yamaha', 26.799);
 //Exemplo Real
 //Quando mudamos a propriedade seletor, o objeto DOM irá passar a selecionar o novo seletor
 //em seus métodos.
-const Dom = {
-  seletor: 'li',
-  element(){
-    return document.querySelector(this.seletor);
-  },
-  ativo(){
-    this.element().classList.add('ativo');
-  },
+// const Dom = {
+//   seletor: 'li',
+//   element(){
+//     return document.querySelector(this.seletor);
+//   },
+//   ativo(){
+//     this.element().classList.add('ativo');
+//   },
+// }
+
+//Dom.ativo(); //adiciona ativo ao 'li'
+//Dom.seletor = 'ul';
+//Dom.ativo(); //adiciona ativo ao 'ul'
+
+
+//Constructor Function Real
+//Um objeto criado com uma 'Constructor', não irá influenciar em outro objeto criado com
+//a mesma 'Constructor'.
+//Função Contrutora (Modo Correto)
+// function Dom(seletor) {
+//   this.element = function(){
+//     return document.querySelector(seletor);
+//   }
+//   this.ativo = function(){
+//     this.element().classList.add('ativo');
+//   }
+// }
+
+//Como usar a função construtora agora?
+const li = new Dom('li');
+//li.ativo();
+const ul = new Dom('ul');
+const liLastChild = new Dom('li:last-child');
+//liLastChild.ativo();
+//Lembre-se de usar parâmetros
+
+
+
+//Exercicios
+//Transforme o objeto abaixo em uma "Constructor Function"
+const pessoa = {
+  nome: 'Nome Pessoa',
+  idade: 0,
+  andar(){
+    console.log(this.nome + ' andou');
+  }
 }
 
-Dom.ativo(); //adiciona ativo ao 'li'
-Dom.seletor = 'ul';
-Dom.ativo(); //adiciona ativo ao 'ul'
+function Pessoa(nome, idade){
+  this.nome = nome,
+  this.idade = idade,
+  this.andar = function(){
+    console.log(this.nome + ' andou');
+  }
+}
 
 
+//Crie 3 pessoas, João - 20 anos | Maria - 25 anos | Bruno - 15 anos
+const joao = new Pessoa('João', 20)
+console.log(joao);
+joao.andar();
+const maria = new Pessoa('Maria', 25);
+console.log(maria);
+maria.andar();
+const bruno = new Pessoa('Bruno', 15)
+console.log(bruno);
+bruno.andar();
 
+
+//Crie uma "Constructor Function" (DOM) para manipulação de istas de elementos do DOM
+//deve conter as seguintes propriedades e métodos:
+/**
+ * elements, retorna NodeList com os elementos selecionados
+ * addClass(classe), adiciona a classe a todos os elementos
+ * removeClass(classe), remove a classe a todos os elementos
+ */
+
+function Dom(seletor){
+  const elementList = document.querySelectorAll(seletor);
+  this.element = elementList;
+  this.addClass = function(classe){
+    elementList.forEach((element) => {
+      element.classList.add(classe);
+    })
+  }
+  this.removeClass = function(classe){
+    elementList.forEach((element) => {
+    element.classList.remove(classe);
+    })
+  }
+}
+const listaItens = new Dom('li');
+
+const ol = new Dom('ol');
+ol.addClass('ativo');
+
+listaItens.removeClass('li-ativo');
